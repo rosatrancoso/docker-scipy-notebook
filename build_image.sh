0a37f7f3de40d06636225fd79d28515dc497f082
Rosa Trancoso <rosatrancoso@gmail.com>
2021-04-06 16:16:16 +1200

    add wgrib and wgrib2

--- build_image.sh
+++ build_image.sh
@@ -3,12 +3,11 @@
 tag=`git rev-parse --short=8 HEAD`
 echo "tag=$tag"
 #time docker build -t mamba_gis . --no-cache 2>&1  | tee build.log
-# time docker build -t mamba_gis . 2>&1  | tee build.log
+time docker build -t mamba_gis . 2>&1  | tee build.log
 
-docker tag mamba_gis docker-scipy-notebook_scipy_gis:$tag
-docker tag mamba_gis rosatrancoso/docker-scipy-notebook_scipy_gis$tag
-
-docker tag mamba_gis docker-scipy-notebook_scipy_gis
-docker tag mamba_gis rosatrancoso/docker-scipy-notebook_scipy_gis
+# docker tag mamba_gis docker-scipy-notebook_scipy_gis
+# docker tag mamba_gis rosatrancoso/docker-scipy-notebook_scipy_gis
+# docker tag mamba_gis rosatrancoso/docker-scipy-notebook_scipy_gis:$tag
 
 
+#
