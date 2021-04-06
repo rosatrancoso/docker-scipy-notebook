825db07dfa7df7c212b688cf36b817eaf1cff1b3
Rosa Trancoso <rosatrancoso@gmail.com>
2021-04-06 17:42:56 +1200

    typo

--- build_image.sh
+++ build_image.sh
@@ -2,12 +2,12 @@
 
 tag=`git rev-parse --short=8 HEAD`
 echo "tag=$tag"
-#time docker build -t mamba_gis . --no-cache 2>&1  | tee build.log
-time docker build -t mamba_gis . 2>&1  | tee build.log
+#time docker build -t mamba_gis -f Dockerfile.mamba_gis . --no-cache 2>&1  | tee build.log
+time docker build -t mamba_gis -f Dockerfile.mamba_gis . 2>&1  | tee build.log
 
-# docker tag mamba_gis docker-scipy-notebook_scipy_gis
-# docker tag mamba_gis rosatrancoso/docker-scipy-notebook_scipy_gis
-# docker tag mamba_gis rosatrancoso/docker-scipy-notebook_scipy_gis:$tag
+docker tag mamba_gis docker-scipy-notebook_scipy_gis
+docker tag mamba_gis rosatrancoso/docker-scipy-notebook_scipy_gis
+docker tag mamba_gis rosatrancoso/docker-scipy-notebook_scipy_gis:$tag
 
 
 #
